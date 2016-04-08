1db9179f3e3c5abfc8ea480b86bcf5d50e5eae04
HiuFungC <14SIC012U@sit.singaporetech.edu.sg>
2016-04-08 18:09:01 +0800

    added comments for task 3 and 4

--- 2107Project/src/twitterAnalysis/task3DMapper.java
+++ 2107Project/src/twitterAnalysis/task3DMapper.java
@@ -1,3 +1,4 @@
+//Created by Chong Hiu Fung
 package twitterAnalysis;
 
 import java.io.BufferedReader;
@@ -15,18 +16,26 @@ public class task3DMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
 	//hash table for the country code
 		Hashtable<String,String> countryCodes= new Hashtable<>();
 		IntWritable one=new IntWritable(1);
-		//read the cache file for country code and name
+		//This function is run before starting the actual map
 		@Override
 		protected void setup(Mapper<LongWritable,Text,Text,IntWritable>.Context context)
 						throws IOException,InterruptedException{
+				//read the cache file for country code and name
 				BufferedReader br = new BufferedReader(new FileReader("ISO-3166-alpha3.tsv"));
 				String line= null;
+				
 				while(true){
+					//read one line of the document
 					line = br.readLine();
+					//while line is not empty keep reading
 					if(line!=null){
+						//split the word by the tab spacing
 						String parts[]=line.split("\t");
+						//add to hastable country code and country name
 						countryCodes.put(parts[0],parts[1]);
-					}else{
+					}
+					//break at the end of the file
+					else{
 						break;
 					}
 				}
@@ -35,13 +44,17 @@ public class task3DMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
 		@Override
 		protected void map(LongWritable key,Text value,Mapper<LongWritable,Text,Text,IntWritable>.Context context)
 			throws IOException,InterruptedException{
+			//read one row and split by column
 			String[] parts = value.toString().split(",");
+			//get negative reason from column 15
 			String negtivereason = parts[15];
+			//get airline from column 16
 			String airline = parts[16];
+			//get country code from column 10
 			String countryCode = parts[10];
-			//compare the three data to form a key 
+			//check if each column is not null
 			if(airline!=null && negtivereason!=null&&countryCode!=null){
-
+				//if the retrieve 
 				if(negtivereason.equals("CSProblem")||negtivereason.equals("badflight")){		
 					//get the country name from the hash table
 					String countryName= countryCodes.get(countryCode);
